7af888c3c0f3710a2eadd7237260334db7d38851
mozihui <13948391+mozihui@user.noreply.gitee.com>
2024-08-17 11:22:17 +0800

    added custom-modal-popup project

---
+++ src/components/custom-modal-popup/modal-test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main">
+        <button @click="showModalPopup = !showModalPopup">Open Modal Popup</button>
+        <Modal v-if="showModalPopup" :id="'Custom-id'" :header="'Customized Header'" :bod="'Customized body'"
+            :footer="'Customized Footer'" @close="onClose"/>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Modal from './modal.vue';
+
+const showModalPopup = ref(false);
+
+const onClose=()=>{
+    showModalPopup.value=false
+}
+</script>
+
+<style lang="scss" scoped>
+.main{
+    text-align: center;
+}
+</style>
