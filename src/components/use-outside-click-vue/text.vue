490945adb4c0893c76376303d9dc7c71f6af4d03
mozihui <13948391+mozihui@user.noreply.gitee.com>
2024-08-22 15:18:03 +0800

    added use-outside-click-vue project

---
+++ src/components/use-outside-click-vue/text.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="display: flex;align-items: center;justify-content: center;">
+        <button v-if="!showContent" @click="showContent=!showContent">Show Content</button>
+        <div v-else ref="contentRef">
+            <h1>This is random content</h1>
+            <p>
+                Please click outside of this to close this. It won't close if you click inside of this content
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import useOutsideClick from './index.js'
+const showContent=ref(false);
+const contentRef = ref(null);
+useOutsideClick(contentRef,()=>{showContent.value=!showContent.value})
+</script>
